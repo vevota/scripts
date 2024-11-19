bbb738205c1acb9303cc0477399e4ce231d1d105
vevota <33405116+vevota@users.noreply.github.com>
2024-11-19 15:14:48 -0800

    Add files via upload

---
+++ window.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Find the window ID of the "Path of Exile" window
+window_id=$(xdotool search --name "Path of Exile")
+
+# Remove window decorations
+xprop -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x2, 0x0, 0x0"
+
+# Resize the window to 2560x1080
+xdotool windowsize $window_id 2560 1080
+
+# Move the window to the top-left corner of the screen (optional)
+xdotool windowmove $window_id 0 0
